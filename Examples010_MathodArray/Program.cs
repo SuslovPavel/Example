2fddab6cbacb789810c814213c962e8ffd33ba91
SuslovPavel <pavel7511084@gmail.com>
2022-07-18 12:32:31 +0500

    Add MethodArray 10

---
+++ Examples010_MathodArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// Требуется найти элемент массива, равный find
+
+int[] array = { 1,21,33,14,21,16,17,81 };
+
+int n = array.Length;
+int find = 21;
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+         break;
+   
+    }
+    
+    // index = index + 1;
+    index++;
+}
+
