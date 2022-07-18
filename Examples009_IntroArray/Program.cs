44569436a196ef67daffbb572249ea5524959465
SuslovPavel <pavel7511084@gmail.com>
2022-07-18 11:31:16 +0500

    Add Examples009_IntroArray

---
+++ Examples009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// нахождение макс. из 9ти чисел с помощью "Массива"
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11,25,44,52,16,23,78,45,19};
+
+int max = Max ( 
+          Max (array[0], array[1], array[2]), 
+          Max (array[3], array[4], array[5]), 
+          Max (array[6], array[7], array[8]));
+
+
+
+Console.WriteLine (max);
