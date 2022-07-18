ba8e7e7ed69a64f743b02e8b446ef10d9fd42525
SuslovPavel <pavel7511084@gmail.com>
2022-07-18 19:49:50 +0500

    Correct Array Library

--- Examples011_ArrayLibrary/Program.cs
+++ Examples011_ArrayLibrary/Program.cs
@@ -1,6 +1,6 @@
-﻿// метод заполнения массива и выведение на экран
+﻿// метод заполнения массива и выведение на экран созданого массива и искомого числа (4)
 
-void FillArray (int[] collection)
+void FillArray (int[] collection) //
  
 {
     int length = collection.Length;
@@ -23,7 +23,33 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;  // вместо 0 можно вписать (-1) тогда в случае если искомое число (например 444) не входит в массивто выводится -1
+
+    while (index < count)
+    {
+       if (collection [index] == find)
+       {
+        position = index;
+        break;                         // прерывание цикла после первой найденой 4ки
+       } 
+        
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10];
 
 FillArray(array);
+
+array[4] = 4; // искуственно присвоили 4 на 4 и 6 индекс
+array[6] = 4;
+
 PrintArray(array);
+
+Console.WriteLine();
+ int pos = IndexOf(array, 4);
+ Console.WriteLine (pos);
