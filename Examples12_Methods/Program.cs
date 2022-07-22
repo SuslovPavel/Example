59aff3f46aba6b3387408ee65d87c2a8ec32d895
SuslovPavel <pavel7511084@gmail.com>
2022-07-22 17:27:18 +0500

    Add Work whith Text in  Examples12_Methods

--- Examples12_Methods/Program.cs
+++ Examples12_Methods/Program.cs
@@ -83,4 +83,32 @@ void Method5()
 }
 //Method5();
 
+//======Работа с текстоМ======//
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а заглавные "С" на маленькие "с".
 
+string text = " - Я думаю, сказал князь, улыбаясь, - что, "
+            + " ежели бы вас послали вместо нашего милого Винценгероде, "
+            + " вы бы взяли приступом согласия прусского колроля. "
+            + " Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = qwerty
+//            012
+// s[3]//r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i =0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+   return result;
+
+}
+String newText = Replace(text, ' ', '-');
+Console.WriteLine(newText);
